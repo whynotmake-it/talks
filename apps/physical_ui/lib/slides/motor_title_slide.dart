281a1d9f2194f6c9acd6ab27af3b226884962e4f
Tim Lehmann <hi@timcreated.it>
2025-08-31 21:55:11 +0200

    fix: motor title slide colors

--- apps/physical_ui/lib/slides/motor_title_slide.dart
+++ apps/physical_ui/lib/slides/motor_title_slide.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:motor_example/title_slide.dart';
+import 'package:wnma_talk/big_quote_template.dart';
 import 'package:wnma_talk/slide_number.dart';
 import 'package:wnma_talk/wnma_talk.dart';
 
@@ -8,9 +10,45 @@ class MotorTitleSlide extends FlutterDeckSlideWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterDeckSlide.custom(
-      builder: (context) => SlideNumber(
-        child: TitleSlideExample(),
+    return BigQuoteTemplate(
+      title: const _Logo(),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTheme(
+      data:
+          CupertinoTheme.of(
+            context,
+          ).copyWith(
+            textTheme: CupertinoTheme.of(context).textTheme.copyWith(
+              textStyle: TextStyle(
+                fontFamily: 'Archivo',
+                color: Color(0xff2F2E2D),
+                fontSize: 64,
+                letterSpacing: .9,
+                fontVariations: const [
+                  FontVariation.weight(500),
+                  FontVariation.width(200),
+                ],
+              ),
+            ),
+          ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MotorLogo(
+              'Motor',
+              visible: true,
+            ),
+          ],
+        ),
       ),
     );
   }
