1d453f46d5c2bbc529929d1f2877804f2536e8dc
Jesper Bellenbaum <jesper@wearetoni.com>
2025-09-21 11:42:30 +0200

    feat: haptic studio slide

--- apps/physical_ui/lib/main.dart
+++ apps/physical_ui/lib/main.dart
@@ -9,7 +9,7 @@ import 'package:physical_ui/slides/design_system_slide.dart';
 import 'package:physical_ui/slides/dimensionality_slides.dart';
 import 'package:physical_ui/slides/glass_slide.dart';
 import 'package:physical_ui/slides/haptic_slide.dart';
-import 'package:physical_ui/slides/history/flat/flat_slides.dart';
+import 'package:physical_ui/slides/haptic_studio_slide.dart';
 import 'package:physical_ui/slides/history/slides/command_line_slide.dart';
 import 'package:physical_ui/slides/history/slides/gui_system1_slide.dart';
 import 'package:physical_ui/slides/history/slides/iphone_notes_physics_slide.dart';
@@ -58,7 +58,26 @@ class PhysicalUiTalk extends StatelessWidget {
             ),
           ),
           slides: [
+            HapticStudioSlide(),
             SummarySurfaceSlide(),
+            DesignSystemSlide(),
+            MotorCardStackSlide(),
+            ShadingAnimationSlide(),
+            ObjectsAndMetaphorsSlide(),
+            CommandLineSlide(),
+            GuiSystem1Slide(),
+            AquaStickiesSlide(),
+            MaterialNotesFlatSlide(),
+            IphoneNotesPhysicsSlide(),
+            BigQuoteTemplate(title: Text("What next?")),
+            ...whatNextSlides,
+
+            BigQuoteTemplate(title: Text("Part 3: Haptic")),
+            HapticSlide(),
+            MotionCharacterSlide(),
+            BigQuoteTemplate(title: Text("Part 2: Audio & Haptics")),
+            AudioCategorySlide(),
+            SoundHapticsSlide(),
             TitleSlide(),
             GlassSlide(),
             HowDidWeGetHereSlide(),
